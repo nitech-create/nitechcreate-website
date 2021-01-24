b8dafedbb5b4db9cf430a9367c47bef0f08f3c04
Sora513 <kokubo.tech@gmail.com>
2021-01-24 19:41:01 +0900

    not interective-@test

--- blog/themes/nitechcreate/source/js/test.js
+++ blog/themes/nitechcreate/source/js/test.js
@@ -15,6 +15,8 @@ function init() {
 
     const ballsize = 30
     const ballstripesize = 0.2;
+    const linesize=200;
+    const lineweight=50
 
 
 
@@ -91,10 +93,53 @@ function init() {
         ballstripe.y = Math.random() * height;
     }
 
+    for (var i = 0; i < linenum; i++) {
+        var line = new createjs.Shape();
+        switch(Math.floor(Math.random()*6)){
+            case 0:
+                line.graphics
+                .beginFill("#F8759D")
+                break;
+            case 1:
+                line.graphics
+                .beginFill("#FDA64A")
+                break;
+            case 2:
+                line.graphics
+                .beginFill("#8CC919")
+                break;
+            case 3:
+                line.graphics
+                .beginFill("#FD9267")
+                break;
+            case 4:
+                line.graphics
+                .beginFill("#FBD000")
+                break;
+            default:
+                line.graphics
+                .beginFill("#00B88E")
+                break;
+
+        }
+var lineweights = (Math.random()+0.5)*lineweight;
+var linesizes=(Math.random()*3+1.5)*linesize;
+        line.graphics.moveTo(linesizes,0)
+        .lineTo(linesizes,lineweights)
+        .lineTo(0,linesizes+lineweights)
+        .lineTo(0,linesizes)
+        .lineTo(linesizes,0)
+
+        line.x = Math.random() * width;
+        line.y = Math.random() * height;
+        stage.addChild(line);
+    }
+
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener('tick', update , false);
 
     
+
     function update(){
         stage.update();
     }
