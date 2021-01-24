2ec82979b9374757a1ea354fe443ac3f95cd4d0c
Sora513 <kokubo.tech@gmail.com>
2021-01-24 21:21:03 +0900

    parallax applied

--- blog/themes/nitechcreate/source/js/indexreveal.js
+++ blog/themes/nitechcreate/source/js/indexreveal.js
@@ -11,3 +11,104 @@ ScrollReveal().reveal('#lottie-logo', {
     viewFactor: 0, // 0~1,どれくらい見えたら実行するか
     reset: false   // 何回もアニメーション表示するか
 });
+
+ScrollReveal().reveal('#indextop', {
+    delay: 3000,
+    opacity: 0,
+    distance: '50px',
+    origin: 'right',
+    duration: 1500, // アニメーションの完了にかかる時間
+    viewFactor: 0, // 0~1,どれくらい見えたら実行するか
+    reset: false   // 何回もアニメーション表示するか
+});
+
+
+ScrollReveal().reveal('#indexsent1', {
+    delay: 3500,
+    opacity: 0,
+    distance: '50px',
+    origin: 'right',
+    duration: 1500, // アニメーションの完了にかかる時間
+    viewFactor: 0, // 0~1,どれくらい見えたら実行するか
+    reset: false   // 何回もアニメーション表示するか
+});
+
+ScrollReveal().reveal('#indexsent2', {
+    delay: 4000,
+    opacity: 0,
+    distance: '50px',
+    origin: 'right',
+    duration: 1500, // アニメーションの完了にかかる時間
+    viewFactor: 0, // 0~1,どれくらい見えたら実行するか
+    reset: false   // 何回もアニメーション表示するか
+});
+
+ScrollReveal().reveal('#indexsent3', {
+    delay: 4500,
+    opacity: 0,
+    distance: '50px',
+    origin: 'right',
+    duration: 1500, // アニメーションの完了にかかる時間
+    viewFactor: 0, // 0~1,どれくらい見えたら実行するか
+    reset: false   // 何回もアニメーション表示するか
+});
+
+ScrollReveal().reveal('#indexsent4', {
+    delay: 5000,
+    opacity: 0,
+    distance: '50px',
+    origin: 'right',
+    duration: 1500, // アニメーションの完了にかかる時間
+    viewFactor: 0, // 0~1,どれくらい見えたら実行するか
+    reset: false   // 何回もアニメーション表示するか
+});
+
+ScrollReveal().reveal('#indexsent5', {
+    delay: 5500,
+    opacity: 0,
+    distance: '50px',
+    origin: 'right',
+    duration: 1500, // アニメーションの完了にかかる時間
+    viewFactor: 0, // 0~1,どれくらい見えたら実行するか
+    reset: false   // 何回もアニメーション表示するか
+});
+
+
+ScrollReveal().reveal('#imgbox1', {
+    delay: 6500,
+    opacity: 0,
+    distance: '50px',
+    origin: 'right',
+    duration: 1500, // アニメーションの完了にかかる時間
+    viewFactor: 0, // 0~1,どれくらい見えたら実行するか
+    reset: false   // 何回もアニメーション表示するか
+});
+ScrollReveal().reveal('#img1', {
+    delay: 6800,
+    opacity: 0,
+    distance: '50px',
+    origin: 'right',
+    duration: 1500, // アニメーションの完了にかかる時間
+    viewFactor: 0, // 0~1,どれくらい見えたら実行するか
+    reset: false   // 何回もアニメーション表示するか
+});
+
+ScrollReveal().reveal('#imgbox2', {
+    delay: 7500,
+    opacity: 0,
+    distance: '50px',
+    origin: 'right',
+    duration: 1500, // アニメーションの完了にかかる時間
+    viewFactor: 0, // 0~1,どれくらい見えたら実行するか
+    reset: false   // 何回もアニメーション表示するか
+});
+
+ScrollReveal().reveal('#img2', {
+    delay: 7800,
+    opacity: 0,
+    distance: '50px',
+    origin: 'right',
+    duration: 1500, // アニメーションの完了にかかる時間
+    viewFactor: 0, // 0~1,どれくらい見えたら実行するか
+    reset: false   // 何回もアニメーション表示するか
+});
