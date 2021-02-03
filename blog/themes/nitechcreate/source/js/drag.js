ea17a57303478eba167969ba0187233b29c3a730
Sora513 <kokubo.t.g.k@gmail.com>
2021-02-03 19:09:47 +0900

    prototype

--- blog/themes/nitechcreate/source/js/drag.js
+++ blog/themes/nitechcreate/source/js/drag.js
@@ -1,4 +1,5 @@
 $(function () {
+    $("#list-about").css("background-color","#800000")
     $('.contents-box').each(function (index, element) {
         $(element).draggable({
             handle: ".data",
@@ -28,6 +29,11 @@ $(function () {
                 topcontent.css("border-color", "#990000")
                 topcontent.children(".data").css("background-color", "#990000")
 
+                $("#taskbar li").each(function () {
+                    $(this).css("background-color", "#994141")
+                })
+                $($(this).attr("data-target")).css("background-color", "#800000")
+
             }
         });
 
@@ -41,19 +47,96 @@ $(function () {
                 }
                 $(this).css("border-color", "#994141")
                 $(this).children(".data").css("background-color", "#994141")
+                $($(this).attr("data-target")).css("background-color", "#994141")
             })
             $(this).css("zIndex", top + 1)
             $(this).css("border-color", "#990000")
             $(this).children(".data").css("background-color", "#990000")
+            $($(this).attr("data-target")).css("background-color", "rgb(128, 0, 0)")
 
 
         })
     })
 
+    $(".contents-box .data .ope").click(function(){
+        
+        if($(this).parent().parent().css("border-color")=="rgb(153, 0, 0)"){
+            var clicked=$(this)
+            setTimeout(function(){$(clicked.parent().parent().attr("data-target")).trigger("click")},100)
+        }else{
+            $($(this).parent().parent().attr("data-target")).trigger("click")
+            var clicked=$(this)
+            setTimeout(function(){$(clicked.parent().parent().attr("data-target")).trigger("click")},100)
+            
+        }
+    })
+
 
     $("#taskbar li").click(function () {
+        var clickedlist = $(this)
         if ($($(this).attr("data-target")).css("visibility") == "visible") {
-            $($(this).attr("data-target")).css("visibility", "hidden")
+            var top = 0;
+            var topcontent
+            $(".contents-box").each(function () {
+                var thisz = parseInt($(this).css('zIndex'), 10)
+                if (thisz > top) {
+                    top = thisz
+                    topcontent = $(this)
+                } else {
+                    if (topcontent === undefined) {
+                        topcontent = $(this)
+                    }
+                }
+            })
+            if (topcontent.attr("data-target") != ("#" + clickedlist.attr("id"))) {
+                $($(this).attr("data-target")).trigger("click")
+            } else {
+                $($(this).attr("data-target")).css("visibility", "hidden")
+                top = 0;
+                topcontent = undefined
+                $('.contents-box').filter(function (index) {
+
+                    return $(this).attr("data-target") != ("#" + clickedlist.attr("id")) && $(this).css("visibility")!="hidden";
+                }).each(function () {
+                    var thisz = parseInt($(this).css('zIndex'), 10)
+                    if (thisz > top) {
+                        top = thisz
+                        if (topcontent === undefined) {
+                        } else {
+                            topcontent.css("border-color", "#994141")
+                            topcontent.children(".data").css("background-color", "#994141")
+                        }
+                        topcontent = $(this)
+                    } else {
+                        if (topcontent === undefined) {
+                            topcontent = $(this)
+                        }else{
+                            $(this).css("border-color", "#994141")
+                            $(this).children(".data").css("background-color", "#994141")
+                        }
+                    }
+                })
+                if(topcontent===undefined){
+
+                }else{
+
+                    topcontent.css("border-color", "#990000")
+                    topcontent.children(".data").css("background-color", "#990000")
+                }
+
+                var visible_exist = false;
+                $("#taskbar li").each(function () {
+                    if ($($(this).attr("data-target")).css("visibility") == "visible") {
+                        visible_exist = true;
+                    }
+                    $(this).css("background-color", "#994141")
+                })
+                if (visible_exist != false) {
+                    $($(topcontent).attr("data-target")).css("background-color", "#800000")
+                }
+            }
+
+
         } else {
             $($(this).attr("data-target")).css("visibility", "visible")
             var top = 0;
@@ -78,11 +161,10 @@ $(function () {
             $($(this).attr("data-target")).css("border-color", "#990000")
             $($(this).attr("data-target")).children(".data").css("background-color", "#990000")
 
-            $("#taskbar li").each(function(){
-                console.log(this)
-                $(this).css("background-color","#994141")
+            $("#taskbar li").each(function () {
+                $(this).css("background-color", "#994141")
             })
-            $(this).css("background-color","#800000")
+            $(this).css("background-color", "#800000")
         }
     })
 });
